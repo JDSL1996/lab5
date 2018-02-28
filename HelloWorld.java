edc7d96c288c9bc9b3ebb6004a966a8ab127e875
Joshua Stephenson-Losey <JDSL1996@hotmail.com>
2018-02-28 15:12:53 -0700

    First Git commit of all time

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package hello.world;
+
+/**
+ *
+ * @author joshlosey
+ */
+public class HelloWorld {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
